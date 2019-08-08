f7a6362fd9aa92118943f9b5e9417c8c9de4c7b1
zhangstar-company <zhangstar666@gmail.com>
2019-08-08 18:17:07 +0800

    Add Eslint in project

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,26 +1,27 @@
-import React, {Component} from "react";
-import {StyleSheet, Text, View} from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 
 export default class HelloWorld extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
                    WebView {URL}
-                </Text>
-            </View>
-        );
-    }
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }, text: {
-        color: 'red',
-        fontSize: 24,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'red',
+    fontSize: 24,
+  },
 });
