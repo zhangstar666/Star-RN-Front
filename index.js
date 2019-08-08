4438a629a364cda32d64ebd93781528cde65e039
zhangstar-company <zhangstar666@gmail.com>
2019-08-08 17:52:39 +0800

    Init project

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import {AppRegistry} from "react-native";
+import HelloWorld from "./src/HelloWorld";
+
+AppRegistry.registerComponent("StarRCT", () => HelloWorld);
---
+++ src/HelloWorld.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import {StyleSheet, Text, View} from 'react-native'
+
+
+export default class HelloWorld extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                   WebView {URL}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }, text: {
+        color: 'red',
+        fontSize: 24,
+    },
+});
