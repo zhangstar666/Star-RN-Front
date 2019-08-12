9c534283b969b4c8f5dfd9d5f395351d5a225f1a
zhangstar-company <zhangstar666@gmail.com>
2019-08-12 18:00:34 +0800

    update react version and react native version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 
-export default class HelloWorld extends Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
